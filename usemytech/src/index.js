87d7854ed80d94fb44ef65752741992378870c9d
Hoolio_Osuna <hoolio.Osuna@gmail.com>
2020-05-27 13:09:10 -0700

    login forms and registrations forms

---
+++ usemytech/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import { Route  } from 'react-router-dom';
+// import RegForm from './components/Register';
+// import Login from './components/Login';
+ReactDOM.render(
+  
+
+    <App />,
+
+ 
+  document.getElementById('root')
+);
+// ReactDOM.render((
+//   <Router >
+//      <Route path = "/" component = {App} >
+//         <Route path = "Login" component = {Login} />
+//         <Route path = "Register" component = {RegForm} />
+//      </Route>
+//   </Router>
+// ), document.getElementById('root')
+// )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
